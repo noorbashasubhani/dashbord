d751b427f3f1b27825946706a5c2360504d4bef7
noorbasha <nbs786786@gmail.com>
2025-02-15 14:52:06 +0530

    git design

--- src/vendorDashbord/pages/LandingPage.jsx
+++ src/vendorDashbord/pages/LandingPage.jsx
@@ -3,17 +3,25 @@ import NavBar from '../components/NavBar'
 import SideBar from '../components/SideBar'
 import VendorLogin from '../components/forms/VendorLogin'
 import VendorRegistration from '../components/forms/VendorRegistration'
-
+import  { useState } from 'react';
 
 const LandingPage = () => {
+
+  const [formType, setFormType] = useState('');
+  const handleFormSwitch = (form) => {
+    setFormType(form);
+  };
   return (
     <>
     <section className="landingSection">
        <NavBar />
        <div className="vendorSection">
        <SideBar />
-      
        </div>
+       <div className="form-container">
+            {formType === 'login' && <VendorLogin />}
+            {formType === 'register' && <VendorRegistration />}
+          </div>
     </section>
     </>
   )
